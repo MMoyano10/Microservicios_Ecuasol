f22bfce20de5dbe6ca4e3b1ace500095436bebac
Brayan Vega <bjvega4@espe.edu.ec>
2025-12-06 11:54:09 -0500

    Limpieza total del repositorio

--- ms-cuentas/cbs/cbs/src/main/java/com/cuentas/productos/cbs/repository/TipoCuentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cuentas.productos.cbs.repository;
-
-import com.cuentas.productos.cbs.entities.TipoCuenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoCuentaRepository extends JpaRepository<TipoCuenta, Integer> {
-}
